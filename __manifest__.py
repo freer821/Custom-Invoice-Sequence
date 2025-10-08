903505e5de7537e740cb59e210c4a7111331aa93
Zhenyu Geng <zhenyu.geng@deutschebahn.com>
2025-10-08 14:00:01 +0200

    update

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
     "depends": ["account"],
     "data": [
         "data/ir_sequence_data.xml",
+        "views/res_company_views.xml",
         "views/res_config_settings_views.xml",
         "views/ir_sequence_views.xml"
     ],
