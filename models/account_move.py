3e8626d8c03a4c1f2f27fe8916200faa8d1ddfd1
Zhenyu Geng <zhenyu.geng@deutschebahn.com>
2025-10-08 13:29:37 +0200

    update

--- models/account_move.py
+++ models/account_move.py
@@ -1,20 +1,18 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _set_next_sequence(self):
-        custom_moves = self.filtered(lambda move: move._needs_custom_invoice_sequence())
-        other_moves = self - custom_moves
-        
-        for move in custom_moves:
-            sequence = move.company_id.invoice_custom_sequence_id
-            sequence_date = move.invoice_date or move.date or fields.Date.context_today(move)
-            move.name = sequence.with_context(ir_sequence_date=sequence_date).next_by_id()
-        
-        if other_moves:
-            super(AccountMove, other_moves)._set_next_sequence()
+    @api.model_create_multi
+    def create(self, vals_list):
+        moves = super().create(vals_list)
+        for move in moves:
+            if move._needs_custom_invoice_sequence():
+                sequence = move.company_id.invoice_custom_sequence_id
+                sequence_date = move.invoice_date or move.date or fields.Date.context_today(move)
+                move.name = sequence.with_context(ir_sequence_date=sequence_date).next_by_id()
+        return moves
 
     def _needs_custom_invoice_sequence(self):
         self.ensure_one()
