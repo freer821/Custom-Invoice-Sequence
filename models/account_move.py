f0014227e25897cb6cd2dc2096ce890ecf0a761c
Zhenyu Geng <zhenyu.geng@deutschebahn.com>
2025-10-08 13:49:54 +0200

    update

--- models/account_move.py
+++ models/account_move.py
@@ -1,4 +1,7 @@
+import logging
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountMove(models.Model):
@@ -6,12 +9,22 @@ class AccountMove(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
+        _logger.info("=== Custom Invoice Sequence: create called ===")
         moves = super().create(vals_list)
         for move in moves:
+            _logger.info(f"Move: {move.name}, Type: {move.move_type}")
+            _logger.info(f"Use custom: {move.company_id.use_custom_invoice_sequence}")
+            _logger.info(f"Sequence ID: {move.company_id.invoice_custom_sequence_id}")
+            _logger.info(f"Current name: '{move.name}'")
+            
             if move._needs_custom_invoice_sequence():
+                _logger.info("=== Applying custom sequence ===")
                 sequence = move.company_id.invoice_custom_sequence_id
                 sequence_date = move.invoice_date or move.date or fields.Date.context_today(move)
                 move.name = sequence.with_context(ir_sequence_date=sequence_date).next_by_id()
+                _logger.info(f"New name: {move.name}")
+            else:
+                _logger.info("=== NOT applying custom sequence ===")
         return moves
 
     def _needs_custom_invoice_sequence(self):
